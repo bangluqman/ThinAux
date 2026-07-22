54bab6334d09691f49b2900ac3d401c0bd9f71db
bangluqman <bang.luqman@gmail.com>
2026-07-22 12:27:43 +0700

    Update model.py

--- model.py
+++ model.py
@@ -2,69 +2,46 @@ import torch
 import torch.nn as nn
 
 
-class DoubleConv(nn.Module):
-    def __init__(self, in_channels: int, out_channels: int):
+class ConvBlock(nn.Module):
+    def __init__(self, in_ch, out_ch):
         super().__init__()
-        self.block = nn.Sequential(
-            nn.Conv2d(in_channels, out_channels, 3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(out_channels, out_channels, 3, padding=1),
-            nn.ReLU(inplace=True),
-        )
+        self.conv1 = nn.Conv2d(in_ch, out_ch, 3, padding=1)
+        self.conv2 = nn.Conv2d(out_ch, out_ch, 3, padding=1)
+        self.relu = nn.ReLU(inplace=True)
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        return self.block(x)
+    def forward(self, x):
+        x = self.relu(self.conv1(x))
+        x = self.relu(self.conv2(x))
+        return x
 
 
-class StandardUNet(nn.Module):
-    def __init__(self, in_channels: int = 3, out_channels: int = 1):
+class UNet(nn.Module):
+    def __init__(self):
         super().__init__()
+        self.c1 = ConvBlock(3, 64)
+        self.c2 = ConvBlock(64, 128)
+        self.c3 = ConvBlock(128, 256)
+        self.c4 = ConvBlock(256, 512)
+        self.bridge = ConvBlock(512, 1024)
+        self.pool = nn.MaxPool2d(2)
+        self.up4 = nn.ConvTranspose2d(1024, 512, 2, 2)
+        self.d4 = ConvBlock(1024, 512)
+        self.up3 = nn.ConvTranspose2d(512, 256, 2, 2)
+        self.d3 = ConvBlock(512, 256)
+        self.up2 = nn.ConvTranspose2d(256, 128, 2, 2)
+        self.d2 = ConvBlock(256, 128)
+        self.up1 = nn.ConvTranspose2d(128, 64, 2, 2)
+        self.d1 = ConvBlock(128, 64)
+        self.out = nn.Conv2d(64, 1, 1)
 
-        self.enc1 = DoubleConv(in_channels, 64)
-        self.pool1 = nn.MaxPool2d(2)
-
-        self.enc2 = DoubleConv(64, 128)
-        self.pool2 = nn.MaxPool2d(2)
-
-        self.enc3 = DoubleConv(128, 256)
-        self.pool3 = nn.MaxPool2d(2)
-
-        self.enc4 = DoubleConv(256, 512)
-        self.pool4 = nn.MaxPool2d(2)
-
-        self.bottleneck = DoubleConv(512, 1024)
-
-        self.up4 = nn.ConvTranspose2d(1024, 512, 2, stride=2)
-        self.dec4 = DoubleConv(1024, 512)
-
-        self.up3 = nn.ConvTranspose2d(512, 256, 2, stride=2)
-        self.dec3 = DoubleConv(512, 256)
-
-        self.up2 = nn.ConvTranspose2d(256, 128, 2, stride=2)
-        self.dec2 = DoubleConv(256, 128)
-
-        self.up1 = nn.ConvTranspose2d(128, 64, 2, stride=2)
-        self.dec1 = DoubleConv(128, 64)
-
-        self.output_conv = nn.Conv2d(64, out_channels, 1)
-
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        x1 = self.enc1(x)
-        x2 = self.enc2(self.pool1(x1))
-        x3 = self.enc3(self.pool2(x2))
-        x4 = self.enc4(self.pool3(x3))
-        xb = self.bottleneck(self.pool4(x4))
-
-        x = self.up4(xb)
-        x = self.dec4(torch.cat([x, x4], dim=1))
-
-        x = self.up3(x)
-        x = self.dec3(torch.cat([x, x3], dim=1))
-
-        x = self.up2(x)
-        x = self.dec2(torch.cat([x, x2], dim=1))
-
-        x = self.up1(x)
-        x = self.dec1(torch.cat([x, x1], dim=1))
-
-        return self.output_conv(x)
+    def forward(self, x):
+        x1 = self.c1(x)
+        x2 = self.c2(self.pool(x1))
+        x3 = self.c3(self.pool(x2))
+        x4 = self.c4(self.pool(x3))
+        x = self.bridge(self.pool(x4))
+        x = self.d4(torch.cat([self.up4(x), x4], 1))
+        x = self.d3(torch.cat([self.up3(x), x3], 1))
+        x = self.d2(torch.cat([self.up2(x), x2], 1))
+        x = self.d1(torch.cat([self.up1(x), x1], 1))
+        return self.out(x)
